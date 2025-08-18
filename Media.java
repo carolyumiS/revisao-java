830670298330dc6efbc83a1a302a0dd1e7d64253
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-18 16:16:01 -0300

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner aluno = new Scanner(System.in);
+
+        System.out.print("Digite sua primeira nota: ");
+        float nota1 = aluno.nextFloat();
+
+        System.out.print("Digite sua segunts nota: ");
+        float nota2 = aluno.nextFloat();
+
+        System.out.print("Digite sua segunts nota: ");
+        float nota3 = aluno.nextFloat();
+
+        float media = (nota1 + nota2 + nota3)/3;
+
+        System.out.printf("Sua média final é:%.2f %n%n", media);
+        aluno.close(); 
+
+        if(media >= 7){
+            System.out.println("Aluno aprovado!");
+        }else if(media >= 5){
+            System.out.println("Aluno em recuperação!");
+        }else{
+            System.out.println("Aluno reprovado!");
+        }
+        
+    }
+}
