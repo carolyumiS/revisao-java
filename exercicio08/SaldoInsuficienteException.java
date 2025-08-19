412cf83ff5a03924dc6c0554bdf95ebdc0173259
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 01:51:44 -0300

    Add files via upload

---
+++ exercicio08/SaldoInsuficienteException.java
@@ -0,0 +1,6 @@
+package ExcecaoPersonalizada;
+public class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
