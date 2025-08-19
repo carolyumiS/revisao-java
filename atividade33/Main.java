930a5032025cbfee014d0448a87231377c3ac595
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 13:40:50 -0300

    Add files via upload

---
+++ atividade33/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MinhaClasseDeTeste testes = new MinhaClasseDeTeste();
+        ExecutorDeTestes.executarTestes(testes);
+    }
+}
