0dee45862df7461651c3970103b8c068edbcf8a9
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 13:41:11 -0300

    Delete atividade31 directory

--- atividade31/TesteAnalisador.java
+++
@@ -1,8 +0,0 @@
-package atividade31;
-
-public class TesteAnalisador {
-    public static void main(String[] args) {
-        Produto p = new Produto(101, "Notebook Gamer", 8500.0);
-        AnalisadordeClasse.inspecionar(p);
-    }
-}
