80074806ddcd93fdbfc649f0c0b8a73e8a929896
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 13:41:28 -0300

    Delete atividade33 directory

--- atividade33/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MinhaClasseDeTeste testes = new MinhaClasseDeTeste();
-        ExecutorDeTestes.executarTestes(testes);
-    }
-}
