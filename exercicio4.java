1af9b159fc695da3d6946dfc40509b16e7ed090f
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 01:42:27 -0300

    Update and rename SomaImpares.java to exercicio4.java

--- exercicio4.java
+++ exercicio4.java
@@ -1,3 +1,4 @@
+//pode ser necessário alterar o nome do arquivo/Classe
 public class SomaImpares {
     public static void main(String[] args) {
         int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
@@ -11,3 +12,4 @@ public class SomaImpares {
         System.out.printf("%nO valor da soma dos números ímpares é: %d %n%n", soma);
     }
 }
+
