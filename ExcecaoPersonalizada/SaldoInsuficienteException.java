fae52593c57a4df112930fcfaedbe8d8f0b50fb8
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2025-08-19 01:50:21 -0300

    Delete ExcecaoPersonalizada directory

--- ExcecaoPersonalizada/SaldoInsuficienteException.java
+++
@@ -1,6 +0,0 @@
-package ExcecaoPersonalizada;
-public class SaldoInsuficienteException extends Exception {
-    public SaldoInsuficienteException(String mensagem) {
-        super(mensagem);
-    }
-}
